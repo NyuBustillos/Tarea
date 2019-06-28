20d6db828a0e278d715c98a075902f7d05fa56e3
Adri_bq <nyubustillos@gmail.com>
2019-06-28 00:28:20 -0400

    Commit Prueba 1 Con emulador

--- app/app/app/ViewModels/ListViewViewModel.cs
+++ app/app/app/ViewModels/ListViewViewModel.cs
@@ -10,7 +10,7 @@ namespace app.ViewModels
         public ObservableCollection<Person>PersonsList { get; set; }
         public ListViewViewModel()
         {
-             PersonsList = new ObservableCollection<Person>
+            PersonsList = new ObservableCollection<Person>
              {
                  new Person() {Name = "Sofia", Age = "1"},
                  new Person() {Name = "Caro", Age = "22"},
@@ -18,7 +18,7 @@ namespace app.ViewModels
                  new Person() {Name = "Néstor", Age = "20"},
                  new Person() {Name = "Copo", Age = "2"},
 
-             }
+             };
         }
         public class Person
         {
