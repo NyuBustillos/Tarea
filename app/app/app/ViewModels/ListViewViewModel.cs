d7795514f5c12db8ef92a915bf428a79178c5b56
Adri_bq <nyubustillos@gmail.com>
2019-06-28 00:14:13 -0400

    Commit Cambios en Main viewModel y listViewviewmodel

---
+++ app/app/app/ViewModels/ListViewViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace app.ViewModels
+{
+    public class ListViewViewModel
+    { 
+        public ObservableCollection<Person>PersonsList { get; set; }
+        public ListViewViewModel()
+        {
+             PersonsList = new ObservableCollection<Person>
+             {
+                 new Person() {Name = "Sofia", Age = "1"},
+                 new Person() {Name = "Caro", Age = "22"},
+                 new Person() {Name = "Adri", Age = "20"},
+                 new Person() {Name = "Néstor", Age = "20"},
+                 new Person() {Name = "Copo", Age = "2"},
+
+             }
+        }
+        public class Person
+        {
+            public string Name { get; set; }
+            public string Age { get; set; }
+
+        }
+    }
+}
---
+++ app/app/app/Views/ListViewPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using app.ViewModels;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace app.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewPage : ContentPage
+    {
+        public ObservableCollection<string> Items { get; set; }
+
+        public ListViewPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new ListViewViewModel();
+        }
+
+       
+    }
+}
